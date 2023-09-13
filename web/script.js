fa5959e74232e5b8f043300c620aad4320e167ed
jwt2706 <jwt2home@gmail.com>
2023-09-13 14:10:36 -0400

    weather options

--- web/script.js
+++ web/script.js
@@ -1,3 +1,12 @@
 function fetchTime() {
     document.getElementById("showTime").innerHTML = new Date();
 }
+
+var slider = document.getElementById("weatherIntensity");
+var output = document.getElementById("weatherIntensityValue");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value + '%';
+}
