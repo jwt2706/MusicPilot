a323cc78060fcc24a503fa0e0ca0b3db8b64109f
jwt2706 <jwt2home@gmail.com>
2024-08-03 11:06:48 -0400

    reorganize

---
+++ app/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='pwa')
+
+@app.route('/')
+def serve_index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/manifest.json')
+def serve_manifest():
+    return send_from_directory(app.static_folder, 'manifest.json')
+
+@app.route('/service-worker.js')
+def serve_service_worker():
+    return send_from_directory(app.static_folder, 'service-worker.js')
+
+@app.route('/assets/<path:path>')
+def serve_assets(path):
+    return send_from_directory(f'{app.static_folder}/assets', path)
+
+@app.route('/icons/<path:path>')
+def serve_icons(path):
+    return send_from_directory(f'{app.static_folder}/icons', path)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- pwa/service-worker.js
+++
@@ -1,6 +0,0 @@
-importScripts('/node_modules/workbox-sw/build/workbox-sw.js');
-
-workbox.routing.registerRoute(
-    ({request}) => request.destination === "image",
-    new workbox.strategies.CacheFirst()
-);
