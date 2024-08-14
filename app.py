a5621c6cfd0311a643622568fb7998e40f19ada7
jwt2706 <jwt2home@gmail.com>
2024-08-14 10:55:50 -0400

    use electron over pwa bc it amkes it a little easier

--- app.py
+++ app.py
@@ -1,28 +1,14 @@
-from flask import Flask, send_from_directory
+from flask import Flask, jsonify
 
-app = Flask(__name__, static_folder='pwa')
+app = Flask(__name__)
 
 @app.route('/')
 def serve_index():
-    return send_from_directory(app.static_folder, 'index.html')
+    return "Hello from Flask!"
 
-@app.route('/manifest.json')
-def serve_manifest():
-    return send_from_directory(app.static_folder, 'manifest.json')
-
-@app.route('/service-worker.js')
-def serve_service_worker():
-    return send_from_directory(app.static_folder, 'service-worker.js')
-
-@app.route('/assets/<path:path>')
-def serve_assets(path):
-    return send_from_directory(f'{app.static_folder}/assets', path)
-
-@app.route('/icons/<path:path>')
-def serve_icons(path):
-    return send_from_directory(f'{app.static_folder}/icons', path)
-
-#@app.route('/api/analyze', methods=['POST']) ....
+@app.route('/api/test')
+def test_api():
+    return jsonify(message="Hello from Flask API!")
 
 if __name__ == '__main__':
     app.run(debug=True)
