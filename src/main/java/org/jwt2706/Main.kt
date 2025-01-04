a15b8efc22de05a75b4de5c0eb68f2e103c03244
jwt2706 <jwt2home@gmail.com>
2025-01-03 20:04:39 -0500

    init

---
+++ src/main/java/org/jwt2706/Main.kt
@@ -0,0 +1,11 @@
+package org.jwt2706
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        System.out.printf("Hello and welcome!")
+        for (i in 1..5) {
+            println("i = $i")
+        }
+    }
+}
